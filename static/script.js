b9c889cf9daa1b418a67e7d5f704ea9d884b2556
mixma5ter <deejay999@yandex.ru>
2024-11-27 15:40:04 +0300

    added api timer

--- static/script.js
+++ static/script.js
@@ -1,129 +1,38 @@
-let timerInterval;
-let startTime;
-let isRunning = false;
-let isReverse = false;
-let elapsedTime = 0;
-
-const clockDisplay = document.getElementById('clock');
-const minutesInput = document.getElementById('minutesBox');
-const secondsInput = document.getElementById('secondsBox');
-const resetButton = document.getElementById('resetButton');
-const startButton = document.getElementById('startButton');
-const pauseButton = document.getElementById('pauseButton');
-const reverseCheckbox = document.getElementById('reverseCheckbox');
-const add30sButton = document.getElementById('add30sButton');
-const subtract30sButton = document.getElementById('subtract30sButton');
-const add1mButton = document.getElementById('add1mButton');
-const subtract1mButton = document.getElementById('subtract1mButton');
-const add5mButton = document.getElementById('add5mButton');
-const subtract5mButton = document.getElementById('subtract5mButton');
-
-
-function updateTimerDisplay() {
-    let currentTime = 0
-
-    if (isReverse && startTime) {
-        currentTime = startTime - Date.now()
-    } else {
-        currentTime = elapsedTime;
-    }
-
-    const minutes = Math.floor(currentTime / 60000);
-    const seconds = Math.floor((currentTime % 60000) / 1000);
-    const displayMinutes = isReverse ? Math.max(0, minutes) : minutes; // не показывать отрицательные значения
-    const displaySeconds = isReverse ? Math.max(0, seconds) : seconds;
-
-    clockDisplay.textContent = `${String(displayMinutes).padStart(2, '0')}:${String(displaySeconds).padStart(2, '0')}`;
+function sendAction(action) {
+    fetch('/timer?action=' + action, { method: 'POST' })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('timer').textContent = data.time;
+        });
 }
 
-function startTimer() {
-    if (!isRunning) {
-        isReverse = reverseCheckbox.checked;
+function resetWithValues() {
+    let minutes = document.getElementById('minutes').value;
+    let seconds = document.getElementById('seconds').value;
 
-        if (isReverse) {
-            if (!startTime) { //Если startTime не задан - задаем его.
-                startTime = Date.now() + elapsedTime;
-            }
-        } else {
-            if (!startTime) { //Если startTime не задан - задаем его.
-                startTime = Date.now() - elapsedTime;
-            }
-        }
+    // Проверка на корректность ввода. Ограничиваем от 0 до 60
+    minutes = Math.min(Math.max(0, minutes), 60);
+    seconds = Math.min(Math.max(0, seconds), 60);
 
-        timerInterval = setInterval(updateTimer, 10);
-        isRunning = true;
-
-        if (startButton) {
-            startButton.classList.add('blinking');
-        }
-    }
+    fetch(`/timer?action=reset&minutes=${minutes}&seconds=${seconds}`, {method: 'POST'})
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('timer').textContent = data.time;
+        });
 }
 
-function pauseTimer() {
-    if (isRunning) {
-        clearInterval(timerInterval);
-        isRunning = false;
-        elapsedTime = Math.abs(Date.now() - startTime); // Сохраняем прошедшее время
-        startTime = null;
-        if (startButton) {
-            startButton.classList.remove('blinking');
-        }
-    }
+function modifyTime(operation, value) {
+    fetch(`/timer?action=${operation}${value}`, { method: 'POST' })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('timer').textContent = data.time;
+        });
 }
 
-function resetTimer() {
-    clearInterval(timerInterval);
-    isRunning = false;
-    startTime = null;
-    if (startButton) {
-        startButton.classList.remove('blinking');
-    }
-    const minutes = parseInt(minutesInput.value) || 0;
-    const seconds = parseInt(secondsInput.value) || 0;
-    elapsedTime = (minutes * 60 + seconds) * 1000;
-    isReverse = reverseCheckbox.checked;
-
-    updateTimerDisplay();
-}
-
-function updateTimer() {
-    if (isReverse) {
-        elapsedTime = startTime - Date.now();
-        if (elapsedTime <= 0) {
-            pauseTimer()
-            elapsedTime = 0;
-            updateTimerDisplay();
-            return;
-        }
-    } else {
-        elapsedTime = Date.now() - startTime;
-    }
-    updateTimerDisplay();
-}
-
-function addTime(seconds) {
-    elapsedTime += seconds * 1000;
-    updateTimerDisplay();
-}
-
-function subtractTime(seconds) {
-    elapsedTime -= seconds * 1000;
-    if (elapsedTime < 0) {
-        elapsedTime = 0;
-    }
-    updateTimerDisplay();
-}
-
-resetButton.addEventListener('click', resetTimer);
-startButton.addEventListener('click', startTimer);
-pauseButton.addEventListener('click', pauseTimer);
-
-add30sButton.addEventListener('click', () => addTime(30));
-subtract30sButton.addEventListener('click', () => subtractTime(30));
-add1mButton.addEventListener('click', () => addTime(60));
-subtract1mButton.addEventListener('click', () => subtractTime(60));
-add5mButton.addEventListener('click', () => addTime(300));
-subtract5mButton.addEventListener('click', () => subtractTime(300));
-
-// Инициализация таймера при загрузке страницы
-resetTimer();
+setInterval(() => {
+    fetch('/timer')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('timer').textContent = data.time;
+        });
+}, 100);
